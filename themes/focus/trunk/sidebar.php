b1c5da4b136529fea3c9ff5fd9eae828897c2e3a
gregpriday <gregpriday@965b8dba-cb3b-494d-9706-4adfdc746276>
2013-05-25 11:19:06 +0000

    Fixed sidebar

--- themes/focus/trunk/sidebar.php
+++ themes/focus/trunk/sidebar.php
@@ -9,8 +9,8 @@
 
 <div id="secondary" class="widget-area" role="complementary">
 	<?php do_action( 'before_sidebar' ); ?>
-	
-	<?php if ( ! dynamic_sidebar( 'sidebar-' . is_page() ? 'page' : 'post' ) ) : ?>
+
+	<?php if ( ! dynamic_sidebar( 'sidebar-' . ( is_page() ? 'page' : 'post' ) ) ) : ?>
 
 		<aside id="search" class="widget widget_search">
 			<?php get_search_form(); ?>
